99be47c7292b413f42c5abe92b4565772b1da7f5
Hugoneytor <halvarez0@ucol.mx>
2020-02-12 08:33:48 -0600

    2 practicas

---
+++ prograWeb/feb_12/funciones/app.js
@@ -0,0 +1,43 @@
+//function statement
+// function greet(){
+//     console.log(`hi`);
+// }
+
+// greet();
+
+//functions are first-class
+// function logGreeting(fn){
+//     fn();
+// }
+
+
+// logGreeting(greet);
+
+
+// let logGreeting = fn =>{
+//     fn();
+// }
+// logGreeting(greet);
+
+let logGreeting = (()=>{
+    console.log('hi');
+});
+
+logGreeting(()=>{
+    console.log(`Hi`);
+});
+
+
+//NOTAS:
+//function expression
+
+// arrow functions
+//string literals
+//first - lass functions
+//function expressions on the fly
+//refactor
+//codereview
+
+//modules (private) - module patterns > expor > function, obj, const, nada
+//require
+//exports
