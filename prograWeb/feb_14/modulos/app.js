409202b02f24db8dad26bd152b63be21409c5197
Hugoneytor <halvarez0@ucol.mx>
2020-02-14 08:41:26 -0600

    Segundo dia de practicas xd

--- prograWeb/feb_14/modulos/app.js
+++ prograWeb/feb_14/modulos/app.js
@@ -1,7 +1,19 @@
 let a = require('./greet');
-let b = require('./greet2').greet;
-let c = require('./greet3');
-
 a();
+
+let b = require('./greet2').greet;
 b();
+
+let c = require('./greet3');
 c.greet();
+c.greeting = 'Changed hello world';
+
+let greet3b = require('./greet3');
+greet3b.greet();
+
+let Greet4 = require('./greet4');
+let gtr = new Greet4();
+gtr.greet();
+
+
+
